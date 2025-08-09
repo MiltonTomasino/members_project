4e36ee76617fd412094a0d0b37e394a55d952b39
MiltonTomasino <jonathantomasino13@gmail.com>
2025-08-08 22:50:37 -0700

    Create Basic Login Form

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 app.use(session({
     store: new pgSession({
         pool: pool,
